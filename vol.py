94edd090e043e4fcb66e27b27c932093dd2cd378
matteofuligni <79851638+matteofuligni@users.noreply.github.com>
2022-01-20 23:08:20 +0100

    Update vol.py
    loading of settings

--- vol.py
+++ vol.py
@@ -1,8 +1,15 @@
 import configparser
+import numpy as np
 
 
+# Loading of settings
 config = configparser.ConfigParser()
 config.read('configuration.txt')
 
 S0 = config.get('settings', 'S0')
 R = config.get('settings', 'R')
+VOL = config.get('settings', 'VOL')
+t_int = config.get('settings', 't_int')
+k_int = config.get('settings', 'k_int')
+N_SIM = config.get('settings', 'N_SIM')
+N_INTER = config.get('settings', 'N_INTER')
