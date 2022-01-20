b06a2ac68a31e98ac2c1f0dbccb4d03c58c0205e
matteofuligni <79851638+matteofuligni@users.noreply.github.com>
2022-01-20 22:39:28 +0100

    Update
    minor changes

--- vol.py
+++ vol.py
@@ -1,8 +1,8 @@
 import configparser
-from sys import argv
+
 
 config = configparser.ConfigParser()
-config.read(sys.argv[1])
+config.read('configuration.txt')
 
 S0 = config.get('settings', 'S0')
 R = config.get('settings', 'R')
