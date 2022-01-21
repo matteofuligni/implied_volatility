12dae407a99843d4afb838c8dd073c3a6e89cc73
matteofuligni <79851638+matteofuligni@users.noreply.github.com>
2022-01-21 15:06:32 +0100

    Update vol.py
    Added the chart

--- vol.py
+++ vol.py
@@ -29,3 +29,34 @@ CC = func.price(T_array, K_array, N_INTER, N_SIM, S0, R, VOL, CC)
 for i in range(len(T_array)):
     for j in range(len(K_array)):
         VV[i,j] = func.find_imp_vol(CC[i,j], S=S0, K=K_array[j], T=T_array[i], r=R)
+
+
+# Price Chart
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(KK, TT, CC, facecolors=cm.jet(CC), lw=0)
+plt.locator_params(axis="x", nbins=5)
+plt.locator_params(axis="y", nbins=5)
+plt.locator_params(axis="z", nbins=5)
+ax.set_xlabel('Strike', fontsize='14')
+ax.set_ylabel('Time', fontsize='14')
+ax.set_zlabel('Price', fontsize='14')
+ax.tick_params(axis='both', which='major', labelsize=10)
+plt.title('Price Chart', fontsize='24')
+plt.savefig('Price.png', dpi = 300)
+plt.show()
+
+# Volatility Chart
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(KK, TT, VV, facecolors=cm.jet(VV), lw=0)
+plt.locator_params(axis="x", nbins=5)
+plt.locator_params(axis="y", nbins=5)
+plt.locator_params(axis="z", nbins=5)
+ax.set_xlabel('Strike', fontsize='14')
+ax.set_ylabel('Time', fontsize='14')
+ax.set_zlabel('Vol', fontsize='14')
+ax.tick_params(axis='both', which='major', labelsize=10)
+plt.title(' Volatility Chart', fontsize='24')
+plt.savefig('Vol.png', dpi = 300)
+plt.show()
